1dfbcc526cbb713c5f59ccfc1c42d50643ff557d
’Vvaraprasad‘ <varaprasadvellanki0402@gmail.com>
2025-02-07 17:20:39 +0530

    back end API

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://fullstack-chatapp-12.onrender.com/api',
   withCredentials: true,
 });
 // import axios from 'axios';
