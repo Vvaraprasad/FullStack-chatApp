789396d7bb91c92ad2b00fe79da5ec1ac4a2eda4
’Vvaraprasad‘ <varaprasadvellanki0402@gmail.com>
2025-02-07 22:06:03 +0530

    updating in jwt

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -55,7 +55,7 @@ export const useAuthStore = create((set, get) => ({
       });
       set({ authUser: res.data });
       toast.success('Logged in successfully');
-
+      
       get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
