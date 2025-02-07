f6966b83ccf6fd6858e9354fba684102fe389471
’Vvaraprasad‘ <varaprasadvellanki0402@gmail.com>
2025-02-07 15:29:19 +0530

    updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,15 +1,15 @@
-// import axios from "axios"
-
-// export const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:5001/api',
-//   withCredentials: true,
-// });
-import axios from 'axios';
+import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === 'development'
-      ? 'http://localhost:5001/api'
-      : '/api',
+  baseURL: 'http://localhost:4000/api',
   withCredentials: true,
 });
+// import axios from 'axios';
+
+// export const axiosInstance = axios.create({
+//   baseURL:
+//     import.meta.env.MODE === 'development'
+//       ? 'http://localhost:5001/api'
+//       : '/api',
+//   withCredentials: true,
+// });
